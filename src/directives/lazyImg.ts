04ebe6db4033926fa9c3032d88e2be2c64eb2bf5
youseonh <dbtjs0221@gmail.com>
2022-05-19 17:43:13 +0900

    주석 추가

--- src/directives/lazyImg.ts
+++ src/directives/lazyImg.ts
@@ -1,10 +1,15 @@
 /**
  *  이미지 파일 lazyLoding
+ *  초기에 이미지를 셋팅할때, 위와같이 구성합니다. 이미지가 실제 안보이는 영역에는 랜더링이 되어있지 않기 때문에 성능이 좋습니다.
  *  @Example v-lazyImg = "UserInfo.pictures"
+ *  https://velog.io/@katanazero86/vue-%EC%82%AC%EC%9A%A9%EC%9E%90-%EC%A0%95%EC%9D%98-%EC%A7%80%EC%8B%9C%EC%9E%90%EB%A5%BC-%ED%99%9C%EC%9A%A9%ED%95%9C-image-lazy-loading-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
  */
 
 import type { App, Directive, DirectiveBinding } from "vue";
 
+//단순 scroll을 감지하여 사용할 수도 있지만, 성능적인 문제로 Intersection Observer 사용을 권장한다.
+// 교차 관찰자 API(Intersection Observer API)는 상위요소 또는 최상위 문서의 뷰포트
+// (viewport)와 대상 요소의 교차점에서 변화를 비동기적으로 관찰할 수 있는 방법을 제공한다
 import { useIntersectionObserver } from "@vueuse/core";
 
 import defaultImg from "/@/assets/images/logo.png";
--- src/router/router.config.ts
+++ src/router/router.config.ts
@@ -5,6 +5,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: () =>
+      // 아래 코드처럼 작성할 시 별도의 chunk 파일을 생성하여 실제 이 라우트를 방문했을 때 리소스를 로드하게 됨
       import(
         /* webpackChunkName: "inputDemo" */ "/@/views/BasicLayout/layout.vue"
       ),
